c0baecebe703367a38f402f77022d84ae1acdc0d
loai esam <112000loai@gmail.com>
2023-09-08 21:48:43 +0300

    5th file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
  * Description: this file will print the alphabet without q and e
  *
- * Return 0 (Success)
+ * Return Always 0 (Success)
 */
-
 int main(void)
 {
-	/* Code here */
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
-	if (alphabet == 'e' || alphabet == 'q')
-		alphabet++;
-	else
+		if (alphabet == 'e' || alphabet == 'q')
+			alphabet++;
 		putchar(alphabet);
-	alphabet++;
+		alphabet++;
 	}
-
 	putchar('\n');
 
-	/* The return value of 0 indicates successful program execution. */
 	return (0);
 }
