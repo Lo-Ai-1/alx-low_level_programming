72d9a1e7af0f82a1bf185d2fc232b2c519f4044e
loai esam <112000loai@gmail.com>
2023-09-08 05:00:33 +0300

    6th files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C Program print the size
+ * Description: using sizof func to print the size
  *
  * Return: Alawys 0 (Success)
 */
+
 int main(vaid)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
