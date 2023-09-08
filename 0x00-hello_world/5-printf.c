457b423c385904086b3f7fe484a07e46f5ace7ff
loai esam <112000loai@gmail.com>
2023-09-08 04:38:59 +0300

    fifth files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 /**
  * main - A prog that show hello
+ *
+ * Description: A C program say hello
+ *
  * Return: 0 (success)
- */
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
