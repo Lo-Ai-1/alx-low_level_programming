cf001d0add78a71918d40b3759c8a40bca9f48a0
loai esam <112000loai@gmail.com>
2023-09-12 21:13:18 +0300

    2ed task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Prints the lowercase alphabet
  *
- * Description: prnt all nums to 9
- *
- * Return Always 0 (Success)
-*/
+ * Description: This function prints all the lowercase letters of the alphabet
+ * in ascending order, followed by a newline character.
+ */
 void print_alphabet(void)
 {
 	int al;
-	for(al = 'a';al <= 'z';al++)
-		_putchar('al');
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		_putchar(al);
+	}
 	_putchar('\n');
 }
