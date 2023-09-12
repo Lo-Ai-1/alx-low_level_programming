fa2e82909cbb1a5e3b4e4c7d323c69595c6bfae5
loai esam <112000loai@gmail.com>
2023-09-12 15:47:11 +0300

    first program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry pointer
+ *
+ * Description: this program print a sentence
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+* this will contain the description of my project.
