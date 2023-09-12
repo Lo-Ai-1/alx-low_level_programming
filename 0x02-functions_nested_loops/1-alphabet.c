9fe290381f2669edd7e6405e8c189562fa83964c
loai esam <112000loai@gmail.com>
2023-09-12 20:55:18 +0300

    2ed task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prnt all nums to 9
+ *
+ * Return Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int al;
+	for(al = 'a';al <= 'z';al++)
+		_putchar('al');
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar (char w);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
