a7850a0bff88017ee5c22b1cb36264705df55657
loai esam <112000loai@gmail.com>
2023-09-14 20:14:40 +0300

    4th task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char w);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
 
 #endif /* MAIN_H */
