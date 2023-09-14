deead510b5f9315276a98ef23822c18c84d56050
loai esam <112000loai@gmail.com>
2023-09-14 21:15:43 +0300

    sdf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			printf("%d", j);
+			if (j >= 10)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
