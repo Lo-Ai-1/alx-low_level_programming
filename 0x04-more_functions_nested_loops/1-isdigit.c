baf96e19120749f03f46b6eaf5366cbe28592c43
loai esam <112000loai@gmail.com>
2023-09-14 19:16:42 +0300

    2nd task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - This checks for a digit
+ *
+ * @c: This is a digit
+ *
+ * Description: function that checks for a digit (0 through 9).
+ * Returns 1 if c is a digit 0 otherwise
+ *
+ * R: This return 1 and 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
