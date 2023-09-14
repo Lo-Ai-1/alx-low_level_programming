e5a499c852f6924b8aca3d76da655b3b6c02ea9f
loai esam <112000loai@gmail.com>
2023-09-14 20:36:03 +0300

    sfdf

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - This is a function that prints the numbers,
+ * from 0 to 9.
+ *
+ * Description: This function prints the numbers from 0 to 9,
+ * excluding the numbers 2 and 4,
+ * followed by a new line.
+ *
+ * Return: This function does not return a value (void).
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i != 2) && (i != 4))
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
+
